a7d3d57da5e99677a4ca918fc977e90d94e15b0d
Kees Jongenburger <jongenburger@riscure.com>
2023-10-19 22:17:25 +0200

    Document the tweaks and add rainbow cvs tables

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,7 +1,11 @@
+--
+-- Tweak flash.nvim to not take over the s keys
+-- as s stands for substiture and not search
+--
 return {
-	"folke/flash.nvim",
-	keys = {
-		-- disable the default flash keymap
-		{ "s", mode = { "n", "x", "o" }, false },
-	},
+  "folke/flash.nvim",
+  keys = {
+    -- disable the default flash keymap
+    { "s", mode = { "n", "x", "o" }, false },
+  },
 }
---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,22 @@
+--
+-- Tweak neo-tee to show more stuff like the build directory
+--
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_by_name = {
+          -- '.git',
+          -- '.DS_Store',
+          -- 'thumbs.db',
+        },
+        never_show = {},
+      },
+    },
+  },
+}
