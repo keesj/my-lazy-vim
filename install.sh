9f5ede473e6a42fea64cc6feb1f5125c0ee48373
Kees Jongenburger <kees.jongenburger@gmail.com>
2023-10-26 12:52:10 +0200

    make opt dir if not present

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # curl -o- https://raw.githubusercontent.com/keesj/my-lazy-vim/keesj/install.sh | bash
+mkdir -p opt
 rm -rf ~/opt/nvim-linux
 wget -qO- https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz | tar zxvf - -C ~/opt
 if ! grep -q '${HOME}/bin' ~/.bashrc
@@ -24,3 +25,8 @@ rm -rf ~/.local/state/nvim
 rm -rf ~/.cache/nvim
 
 git clone https://github.com/keesj/my-lazy-vim.git ~/.config/nvim
+echo "## COPY / PASE in root shell"
+echo
+echo sudo apt-get install tmux ripgrep moreutils build-essential silversearcher-ag python3-venv curl wget
+echo python3 -mvenv .venv
+echo 'export PATH="${HOME}/.vent/bin:${PATH}"'
