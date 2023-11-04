794924af755edbb20f2931d7405a0b3378e14299
Kees Jongenburger <jongenburger@riscure.com>
2023-11-04 08:07:49 +0100

    Add TODO list

--- install.sh
+++ install.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
 # curl -o- https://raw.githubusercontent.com/keesj/my-lazy-vim/keesj/install.sh | bash
+
+# TODO: Add Check git / curl persent
+# TODO: Check for exising install (preffer git pull v.s. clone)
+# TODO: Diff before updating
+# TODO: Fail early in the script if it fails
+# TODO: Bail out of first error
+# TODO: Add extra script next to nvim to update itself
+# TODO: Allow to switch from http checkout to checkout over ssh e.g. git remote add rw  git@github.com:keesj/my-lazy-vim.git
+# TODO: Add wget syntax
 mkdir -p opt
 rm -rf ~/opt/nvim-linux
 wget -qO- https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz | tar zxvf - -C ~/opt
-if ! grep -q '${HOME}/bin' ~/.bashrc
-then
-        echo 'export PATH="${HOME}/bin:${PATH}"' >> ~/.bashrc
+if ! grep -q '${HOME}/bin' ~/.bashrc; then
+	echo 'export PATH="${HOME}/bin:${PATH}"' >>~/.bashrc
 fi
-if [ ! -d bin ]
-then
-        mkdir bin
+if [ ! -d bin ]; then
+	mkdir bin
 fi
-if [ ! -e bin/nvim ]
-then
-        rm -rf bin/nvim
-        ln -s ../opt/nvim-linux64/bin/nvim bin/nvim
+if [ ! -e bin/nvim ]; then
+	rm -rf bin/nvim
+	ln -s ../opt/nvim-linux64/bin/nvim bin/nvim
 fi
 
 rm -rf ~/.config/nvim
