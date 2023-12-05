2186aacc39a1231dbebd5d0d62c00c2de7f71440
Kees Jongenburger <jongenburger@riscure.com>
2023-12-05 10:14:58 +0100

    Fix a few todo items and move .bashrc adding outside of the nvim install

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # curl -o- https://raw.githubusercontent.com/keesj/my-lazy-vim/keesj/install.sh | bash
-
-# TODO: Add Check git / curl persent
+# wget -qO- https://raw.githubusercontent.com/keesj/my-lazy-vim/keesj/install.sh | bash
+#
+#
 TOOLS="git tar wget"
 for i in $TOOLS; do
 	if ! type $i 1>/dev/null 2>/dev/null; then
@@ -10,26 +11,29 @@ for i in $TOOLS; do
 		exit 1
 	fi
 done
-# TODO: Check for exising install (preffer git pull v.s. clone)
 # TODO: Diff before updating
 # TODO: Fail early in the script if it fails
 # TODO: Bail out of first error
 # TODO: Add extra script next to nvim to update itself
 # TODO: Allow to switch from http checkout to checkout over ssh e.g. git remote add rw  git@github.com:keesj/my-lazy-vim.git
-# TODO: Add wget syntax
+#
 mkdir -p opt
 if uname -p | grep x86_64 2>&1 >/dev/null; then
 	rm -rf ~/opt/nvim-linux
 	wget -qO- https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz | tar zxvf - -C ~/opt
-	if ! grep -q '${HOME}/bin' ~/.bashrc; then
-		echo 'export PATH="${HOME}/bin:${PATH}"' >>~/.bashrc
-		echo 'alias vim="nvim"' >>~/.bashrc
-	fi
 else
 	echo "Skip nvim install"
 	echo https://github.com/neovim/neovim
 	sleep 5
 fi
+
+# Add nvim to the path and create aliaes if needed
+if ! grep -q '${HOME}/bin' ~/.bashrc; then
+	echo 'export PATH="${HOME}/bin:${PATH}"' >>~/.bashrc
+	echo 'alias vim="nvim"' >>~/.bashrc
+	echo 'alias vi="nvim"' >>~/.bashrc
+fi
+
 if [ ! -d bin ]; then
 	mkdir bin
 fi
