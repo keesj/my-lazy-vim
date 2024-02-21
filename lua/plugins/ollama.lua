893a1c43da55488d84c1374778ee20b69984d9cd
Kees Jongenburger <jongenburger@riscure.com>
2024-02-21 10:55:31 +0100

    Add ollama

--- lua/plugins/ollama.lua
+++ lua/plugins/ollama.lua
@@ -27,18 +27,38 @@ return {
 
   ---@type Ollama.Config
   opts = {
-    model = "mistral",
+    model = "mixtral",
+    --url = "http://10.0.3.1:11435",
     --model = "codellama",
     --model = "codellama:7b-code-q4_0",
     --model = "keesj",
     -- your configuration overrides
+    --        prompt = "Rewrite as Riscure security analyst: ceives $input and $sel(ection), among others.",
     prompts = {
-      Sample_Prompt = {
-        prompt = "This is a sample prompt that receives $input and $sel(ection), among others.",
+      AA_Rewrite_Prof_Long = {
+        prompt = "Rewrite the following text to make it look more professional:\n\n $sel",
+        input_label = "> ",
+        model = "mixtral",
+        action = "display",
+      },
+      AA_QuickSecRev = {
+        prompt = "Any obvious security issues in the following code? keep it short:\n\n $sel",
         input_label = "> ",
         model = "mistral",
         action = "display",
       },
+      AA_MistralFuzz = {
+        prompt = "Create a lib fuzzer fuzzing harness for the following code:\n\n $sel",
+        input_label = "> ",
+        model = "mixtral",
+        action = "display",
+      },
+      --      MixtralFuzz = {
+      --        prompt = "Create a lib fuzzer fuzzing harness for the following code: $sel",
+      --        input_label = "> ",
+      --        model = "mixtral",
+      --        action = "display",
+      --      },
     },
   },
 }
